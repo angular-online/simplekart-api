c42b3f93fc4914c373dec436b829c61a6cbf85f7
Nagarjuna Immadi <nagarjuna.immadi@thomsonreuters.com>
2019-04-04 20:10:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var app = require("./app");
+var http = require("http");
+
+var server = http.createServer(app);
+
+var port = process.env.port || 3000;
+
+server.listen(port, function(){
+    console.log("Server running on port "+port);
+});
