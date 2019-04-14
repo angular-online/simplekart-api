dc394001b8a3c700a16768bd5337a885ddc132c2
Nagarjuna <w3.nagarjuna@gmail.com>
2019-04-14 23:06:55 +0530

    updated ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require("http");
 
 var server = http.createServer(app);
 
-var port = process.env.port || 3000;
+var port = process.env.port || 4000;
 
 server.listen(port, function(){
     console.log("Server running on port "+port);
