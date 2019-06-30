3a00af46e3c7867b7355871197cb24d8fec26b7a
Nagarjuna <w3.nagarjuna@gmail.com>
2019-06-30 08:00:19 +0530

    changed env port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require("http");
 
 var server = http.createServer(app);
 
-var port = process.env.port || 4000;
+var port = process.env.PORT || 4000;
 
 server.listen(port, function(){
     console.log("Server running on port "+port);
